9a3a372cf379c94c42b0651fe4fba4ce15276db4
Simon McCallum <simon.mccallum@ecs.vuw.ac.nz>
2021-10-18 15:32:50 +1300

    initial commit

---
+++ app/src/main/java/nz/ac/wgtn/ecs/swen502v2/MainActivity.java
@@ -0,0 +1,63 @@
+package nz.ac.wgtn.ecs.swen502v2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import nz.ac.wgtn.ecs.swen502v2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private int instruction = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+
+    public void changeInstruction(View view) {
+        TextView currentInstruction = findViewById(R.id.instructionText);
+        ImageView currentInstructionImage = findViewById(R.id.instructionImage);
+        if (instruction == 1) {
+            currentInstruction.setText(R.string.instruction2);
+            currentInstructionImage.setImageResource(R.drawable.mobileinhand1);
+            instruction = 2;
+        } else {
+            if (instruction == 2) {
+                currentInstruction.setText(R.string.instruction3);
+                instruction = 3;
+            }
+            else if (instruction == 3){
+                currentInstruction.setText(R.string.instruction1);
+                currentInstructionImage.setImageResource(R.drawable.instruction1);
+                instruction = 1;
+            }
+
+        }
+    }
+}
